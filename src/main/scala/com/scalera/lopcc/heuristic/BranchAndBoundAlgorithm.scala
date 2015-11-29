c5ad913d7e96dcc4cd8f7ee5fe0af73a799b29ab
David Vallejo Navarro <d.vallejo.nava@gmail.com>
2015-11-29 23:48:25 +0100

    First changes

--- src/main/scala/com/scalera/lopcc/heuristic/BranchAndBoundAlgorithm.scala
+++ src/main/scala/com/scalera/lopcc/heuristic/BranchAndBoundAlgorithm.scala
@@ -1,18 +1,61 @@
 package com.scalera.lopcc.heuristic
 
-import com.scalera.lopcc.util.Graph
-import com.scalera.lopcc.problem.Solution
+import com.scalera.lopcc.util.{ Graph, Childrens, QueueNode }
+import com.scalera.lopcc.problem.{ Solution, Bounds }
 
-// import scala.collection.mutable.PriorityQueue
+import scala.collection.mutable.PriorityQueue
 
-object BranchAndBoundAlgorithm extends Algorithm {
+object BranchAndBoundAlgorithm extends Algorithm with Bounds {
 
   def execute(graph: Graph): Solution = {
-    val sol = Solution.empty(graph.maxNumNodes)
     val initBound = getInitBound(graph)
-    branchAndBound(graph, sol, initBound)
+    branchAndBound(graph, initBound)
   }
 
-  def branchAndBound(graph: Graph, sol: Solution, bound: Double): Solution = ???
-  //   val queue = new PriorityQueue.empty[QueueNode] 
+  def branchAndBound(graph: Graph, bound: Double): Solution = {
+    
+    var sol = Solution.empty(graph.maxNumNodes)
+    // val queue = PriorityQueue.empty[QueueNode]
+    // val childrens = Childrens(List.empty[QueueNode])
+    
+    // var partialGraph: Graph = graph
+    // var partialSol: Solution = sol
+    // var boundAux: Double = getLB1(partialGraph, partialSol)
+    // var queueNode: QueueNode = QueueNode(partialGraph, partialSol, boundAux)
+
+    // queue.enqueue(queueNode)
+
+    // while(!queue.isEmpty) {
+      
+    //   queueNode = queue.dequeue
+    //   partialGraph = queueNode.graph
+    //   partialSol = queueNode.sol
+    //   boundAux = queueNode.bound
+
+    //   if (partialSol.isComplete) {
+    //     if (partialSol.isBetter(sol)) {
+    //       sol = partialSol
+    //       boundAux = sol.getCost
+    //     }
+
+    //   } else if(boundAux <= cotaSuperior){  //Podo          
+    //       hijos.compleccionesFactibles(partialSol, grafoParcial)
+    //       while(childrens.isEmpty()){
+    //         nodoCola = hijos.dameHijo()
+    //         nodoCola.getNodo().copiar(partialSol)
+    //         nodoCola.getGrafo().copiar(grafoParcial)
+    //         cotaAux = nodoCola.getCotaSolucion()
+    //         if(cotaAux <= cotaSuperior){ //Podo
+    //           nodoCola = new NodoColaPrioridad(partialSol, cotaAux, grafoParcial)
+    //           cola.add(nodoCola)
+    //         }else{
+    //           nodosPodados = nodosPodados + calcularNodosPodados(partialSol)
+    //         }
+    //       }
+    //   } else nodosPodados = nodosPodados + calcularNodosPodados(partialSol)
+    // }
+
+    sol
+
+  }
 }
--- src/main/scala/com/scalera/lopcc/util/Childrens.scala
+++ src/main/scala/com/scalera/lopcc/util/Childrens.scala
@@ -1,13 +1,26 @@
 package com.scalera.lopcc.util
 
-import com.scalera.lopcc.problem.Solution
+import com.scalera.lopcc.problem.{ Solution, Bounds }
 
-case class Childrens(childrens: List[QueueNode]) {
+case class Childrens(childrens: List[QueueNode]) extends Bounds {
 
   def getChildren: QueueNode = childrens.last
 
   def isEmpty: Boolean = childrens.isEmpty
 
-  def feasibleChildrens(partialSol: Solution, graph: Graph): Solution = ???
+  def feasibleChildrens(partialSol: Solution, graph: Graph): Childrens = {
+
+    val newChildrens = 
+      partialSol.nodes.foldLeft(List.empty[QueueNode]) {
+        case (childrens, node) =>
+          val newGraph = graph.removeNode(node)
+          val newSol = partialSol.insertNode(node, graph)
+
+          childrens :+ QueueNode(newGraph, newSol, getLB1(newGraph, newSol))
+      }
+
+    this.copy(childrens = newChildrens)
+
+  }
 
 }
