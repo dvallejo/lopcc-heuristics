1ef0ef7b86f15746f4a19d7afb93ba7980bef419
David Vallejo Navarro <d.vallejo.nava@gmail.com>
2016-04-06 18:27:28 +0200

    Added new files to pruebas

--- src/main/scala/com/scalera/lopcc/generator/FileGenerator.scala
+++ src/main/scala/com/scalera/lopcc/generator/FileGenerator.scala
@@ -1,35 +1,35 @@
-package com.scalera.lopcc.generator
+// package com.scalera.lopcc.generator
 
-import java.io._
-import scala.util.Random
+// import java.io._
+// import scala.util.Random
 
-object FileGenerator {
+// object FileGenerator {
 
-  val filename = "src/main/resources/pruebas/dvn"
+//   val filename = "src/main/resources/pruebas/dvn"
 
-  def main(args: Array[String]) =
-    args match {
-      case Array(maxNodes) =>
-        (16 to maxNodes.toInt) foreach writeInFile
+//   def main(args: Array[String]) =
+//     args match {
+//       case Array(maxNodes) =>
+//         (15 to maxNodes) foreach writeInFile
 
-      case _ => 
-        println("usage: maxNumNodes")
-    }
+//       case _ => 
+//         println("usage: maxNumNodes")
+//     }
 
-  def writeInFile(numNodes: Int) = {
-    val writer = new PrintWriter(new File(s"$filename$numNodes.txt"))
-    writer.write(createGraph(numNodes))
-    writer.close()
-  }
+//   def writeInFile(numNodes: Int) = {
+//     val writer = new PrintWriter(new File(s"$filename$numNodes.txt"))
+//     writer.write(createGraph(numNodes))
+//     writer.close()
+//   }
 
-  def createGraph(numNodes: Int) =
-    Seq.fill(numNodes + 1)(createRow(numNodes)).mkString("\n")
+//   def createGraph(numNodes: Int) =
+//     Seq.fill(numNodes + 1)(createRow(numNodes)).mkString("\n")
 
-  def createRow(numNodes: Int): String =
-    Seq.fill(numNodes)(generateRandomValue).mkString("  ")
+//   def createRow(numNodes: Int): String =
+//     Seq.fill(numNodes)(generateRandomValue).mkString("  ")
 
-  def generateRandomValue: String = {
-    val random = Random.nextDouble
-    random.toString.take(8)
-  }
-}
+//   def generateRandomValue: String = {
+//     val random = Random.nextDouble
+//     random.toString.take(8)
+//   }
+// }
